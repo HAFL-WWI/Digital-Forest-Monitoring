d55e1843fd95076268a0091b2631547d91d5aa2b
Hanskaspar Frei <hkfrei@karten-werk.ch>
2020-02-18 14:02:47 +0100

    fix:move .git file back to root directory.

---
+++ methods/use-case1/ndvi_max_2018_switzerland.R
@@ -0,0 +1,86 @@
+############################################################
+# Calculate NDVI max composite for Switzerland
+#
+# multi-core processing -> ca. 25min
+#
+# by Dominique Weber, HAFL, BFH
+############################################################
+
+start_time <- Sys.time()
+
+# source functions
+source("/home/wbd3/sentinel2-whff/composites/calc_pixel_composites.R")
+source("/home/wbd3/sentinel2-whff/vegetation_indices/calc_vegetation_indices.R")
+
+# wd
+setwd("//mnt/cephfs/data/HAFL/WWI-Sentinel-2/Data/AnnualComposites/")
+
+# global params
+band_names = c("B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09", "B10","B11", "B12")
+dates_2018_filter = c("201806", "201807")
+
+# 2018 T32TLT
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TLT/2018/"
+ndvi_max_2018_t32tlt = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t32tlt", calc_ndvi, max)
+
+# 2018 T32TLS
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TLS/2018/"
+ndvi_max_2018_t32tls = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t32tls", calc_ndvi, max)
+
+# 2018 T32TMT
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TMT/2018/"
+ndvi_max_2018_t32tmt = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t32tmt", calc_ndvi, max)
+
+# 2018 T32TMS
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TMS/2018/"
+ndvi_max_2018_t32tms = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t32tms", calc_ndvi, max)
+
+# 2018 T32TNT
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TNT/2018/"
+ndvi_max_2018_t32tnt = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t32tnt", calc_ndvi, max)
+
+# 2018 T32TNS
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TNS/2018/"
+ndvi_max_2018_t32tns = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t32tns", calc_ndvi, max)
+
+# 2018 T32TMR
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TMR/2018/"
+ndvi_max_2018_t32tmr = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t32tmr", calc_ndvi, max)
+
+# 2018 T31TGM (-> UTM zone 31!)
+images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T31TGM/2018/"
+ndvi_max_2018_t31tgm = calc_pixel_composites(images_path, band_names, dates_2018_filter, "2018_t31tgm", calc_ndvi, max)
+
+# Problem different utm zones
+# There is no easy solution. We tried projectRaster to UTM32 and to LV95 but then rasters have different origins, resolution or row/col numbers
+
+# The only solution that worked so far is this (but somehow then ArcGIS had problem with the resulting mosaic):
+# reproject, extend and resample
+# ndvi_max_2018_t31tgm_32 = projectRaster(ndvi_max_2018_t31tgm, crs=crs(ndvi_max_2018_t32tls))
+# extend_32tls = extend(ndvi_max_2018_t32tls, ndvi_max_2018_t31tgm_32)
+# ndvi_max_2018_t31tgm_32_resampled = resample(ndvi_max_2018_t31tgm_32, extend_32tls)
+# mosaic all...
+
+# create mosaic
+ndvi_max_2018_ch = mosaic(ndvi_max_2018_t32tlt,
+                          ndvi_max_2018_t32tls, 
+                          ndvi_max_2018_t32tmt, 
+                          ndvi_max_2018_t32tms,
+                          ndvi_max_2018_t32tnt,
+                          ndvi_max_2018_t32tns,
+                          ndvi_max_2018_t32tmr,
+                          fun=max, 
+                          filename="ndvi_max_2018_ch.tif")
+
+# write T31TGM separately
+writeRaster(ndvi_max_2018_t31tgm, "ndvi_max_2018_t31tgm.tif")
+
+print(Sys.time() - start_time)
+
+# TODO
+# Addtional tiles - missing on the BFH server!
+#
+# T32TPT (GR)
+# T32TPS (GR)
+# T32TLR (VS)
+
