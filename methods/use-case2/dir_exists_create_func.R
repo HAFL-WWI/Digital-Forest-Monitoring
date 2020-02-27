3a1cb91b1bf1c759ec706f862f664d2d20b3de22
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-02-27 18:06:05 +0100

    file moved

--- methods/use-case2/dir_exists_create_func.R
+++
@@ -1,9 +0,0 @@
-dir_exist_create = function(main_dir, sub_dir){
-  
-  output_dir = paste(main_dir,sub_dir,sep="")
-  
-  if (!dir.exists(output_dir)){
-    dir.create(output_dir)
-  } 
-  return(output_dir)
-}
