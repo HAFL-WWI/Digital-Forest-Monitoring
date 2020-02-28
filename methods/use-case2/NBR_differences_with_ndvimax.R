d8f305a6f51c9ec2541400cb306f16b84bf5cd46
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-02-28 14:00:25 +0100

    indentation

--- methods/use-case2/NBR_differences_with_ndvimax.R
+++ methods/use-case2/NBR_differences_with_ndvimax.R
@@ -32,5 +32,11 @@ foreach(i=1:length(tile_vec)) %dopar% {
   calc_nbr_differences(main_path, out_path, tile_vec[i], year="2017", ref_date=as.Date("2017-08-09"), time_int_nbr=3, time_int_refstack=20, scl_vec=c(3,5,7:10), cloud_value=-999, nodata_value=-555)
 }
 
+# --> mask, polygonize etc.
+
+foreach(i=1:length(tile_vec)) %dopar% {
+  build_composite_stack(main_path, out_path, tile_vec[i], year="2017", ref_date=as.Date("2017-08-09"), time_int_nbr=3, time_int_refstack=20)
+}
+
 stopCluster(cl)
 print(Sys.time() - start_time)
