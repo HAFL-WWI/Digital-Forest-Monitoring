763f48a46a24ff5e72198d7043fe7dd89ca53761
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-03-03 14:09:33 +0100

    build composite stack function renamed

--- methods/use-case2/build_composite_stack.R
+++ methods/use-case2/build_composite_stack.R
@@ -31,7 +31,7 @@ build_composite_stack = function(main_path, out_path, tile="T32TMT", year="2017"
   nbr_files = list.files(nbr_path)
   dates_nbr = as.Date(substring(lapply(strsplit(nbr_files,"_"), "[[", 3),1,8), format = "%Y%m%d")
   
-  #dates_todo = which(dates_all > max(dates_nbr))
+  #dates_todo = which(dates_all >= max(dates_nbr))
   dates_todo = which((dates_all <= ref_date) & (dates_all >= ref_date-time_int_nbr))
   
   if (length(dates_todo)>0 & length(list.files(comp_path))<length(nbr_files)){
@@ -44,22 +44,25 @@ build_composite_stack = function(main_path, out_path, tile="T32TMT", year="2017"
     comp_stk = foreach(i=length(dates_todo):1, .packages = c("raster"), .combine = "addLayer") %dopar% {
   
       # define dates within time interval
-      date_to_do = as.Date(substring(lapply(strsplit(B8Names[i],"_"), "[[", 3),1,8), format = "%Y%m%d")
+      date_to_do = as.Date(substring(lapply(strsplit(B8Names[dates_todo[i]],"_"), "[[", 3),1,8), format = "%Y%m%d")
       ind_dates = which((dates_all <= date_to_do) & (dates_all >= date_to_do - time_int_refstack))
       dates_for_comp = gsub("-","",dates_all[ind_dates])
   
       # call calc_pixel_comp function
       source("//home/eaa2/Digital-Forest-Monitoring/methods/general/calc_veg_indices.R")
       source("//home/eaa2/Digital-Forest-Monitoring/methods/general/calc_max_composite.R")
-      ndvi_stk = calc_veg_indices (stack_path, "B04_10m", "B08_10m", stk1 = NULL, stk2 = NULL, ndvi_raw_path, dates_for_comp, veg_ind="NDVI", tilename=tile, ext=NULL)
+      
+      b8 = list.files(stack_path, pattern="B08_10m", recursive=T, full.names=T)
+      b8 = b8[grepl(paste(dates_for_comp, collapse="|"), b8)]
+      b12 = list.files(stack_path, pattern="B12_20m", recursive=T, full.names=T)
+      b12 = b12[grepl(paste(dates_for_comp, collapse="|"), b12)]
+      b4 = list.files(stack_path, pattern="B04_10m", recursive=T, full.names=T)
+      b4 = b4[grepl(paste(dates_for_comp, collapse="|"), b4)]
+      
+      ndvi_stk = calc_veg_indices (stk_1 = stack(b8), stk_2 = stack(b4), ndvi_raw_path, dates_for_comp, veg_ind="NDVI", tilename=tile, ext=NULL)
+      nbr_stk = calc_veg_indices (stk_1 = stack(b8), stk_2 = disaggregate(stack(b12),2), nbr_raw_path, dates_for_comp, veg_ind="NBR", tilename=tile, ext=NULL)
   
-      names8 = list.files(stack_path, pattern="B08_10m", recursive=T, full.names=T)
-      names12 = list.files(stack_path, pattern="B12_20m", recursive=T, full.names=T)
-      names8 = names8[grepl(paste(dates_for_comp, collapse="|"), names8)]
-      names12 = names12[grepl(paste(dates_for_comp, collapse="|"), names12)]
-      nbr_stk = calc_veg_indices (NULL, NULL, NULL, stk1 = stack(names8), stk2 = disaggregate(stack(b12),2), nbr_raw_path, dates_for_comp, veg_ind="NBR", tilename=tile, ext=NULL)
-  
-      ind_ras = calc_max_composite (stack_path=NULL, stk=ndvi_stk, dates=NULL, ext=NULL, calc_max=F, calc_ind=T)
+      ind_ras = calc_max_composite (vi_stk=ndvi_stk, ext=NULL, calc_max=F, calc_ind=T)
   
       comp_tmp = stackSelect(nbr_stk, ind_ras)
   
