646ccc9a10b52ea0e9445b5e66b65ed3c9658c2f
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-03-03 13:49:37 +0100

    calc max composite function simplified

--- methods/general/calc_max_composite.R
+++ methods/general/calc_max_composite.R
@@ -6,25 +6,13 @@
 
 #' Pixel composites based on VI (NDVI) max
 #' @return Raster layer (ndvi max or index) or stack (ndvi max, index)
-calc_max_composite <- function(stack_path, stk=NULL, dates, ext=NULL, calc_max=F, calc_ind=T) {
+calc_max_composite <- function(vi_stk, ext=NULL, calc_max=F, calc_ind=T) {
   
   # load packages
   library(raster)
 
-  if (is.null(stk)){
-    # filter dates
-    fileNames = list.files(stack_path, full.names = T)
-    fileNames = fileNames[grepl(paste(dates, collapse="|"), fileNames)]
-
-    # get stack
-    vi_stk = brick(fileNames)
-    }
-  else {
-    vi_stk = stk
-  }
-  
   # crop
-  if (!is.null(vi_stk)) vi_stk = crop(vi_stk, ext)
+  if (!is.null(ext)) vi_stk = crop(vi_stk, ext)
 
   # calc vegetation index max / max index
   if (calc_max){
@@ -34,7 +22,7 @@ calc_max_composite <- function(stack_path, stk=NULL, dates, ext=NULL, calc_max=F
   
   if (calc_ind){ 
     max_ind = which.max(vi_stk)
-    if (!calc_max) return (ind_max)
+    if (!calc_max) return (max_ind)
   }
   
   return(stack(vi_max,max_ind))
