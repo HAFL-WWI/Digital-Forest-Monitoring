a9360687a1d3b3fbc378c31b495e3dce918bd41f
dominiqueweber <domi.wbr@gmail.com>
2020-03-04 11:47:31 +0100

    NDVI-Max scripts for use-case 1 improved

---
+++ methods/use-case1/mosaic.R
@@ -0,0 +1,36 @@
+############################################################
+# Project to LV95 and mosaic all tiles
+#
+# by Dominique Weber, BFH-HAFL
+############################################################
+
+# load packages
+library(foreach)
+library(doParallel)
+
+mosaic <- function(path) {
+  # get files
+  files = list.files(path, pattern = ".tif", full.names = T)
+  
+  # project lv95
+  print(paste("processing", length(files), "tiles in parallel mode..."))
+  cl = makeCluster(detectCores() -1)
+  registerDoParallel(cl)
+  foreach(i=1:length(files)) %dopar% {
+    path = dirname(files[[i]])
+    in_file = files[[i]]
+    out_file = paste(tools::file_path_sans_ext(files[[i]]), "_lv95", ".tif", sep="")
+    cmd = paste("gdalwarp -t_srs EPSG:2056 -tr 10 10", in_file, out_file)
+    system(cmd)
+  }
+  stopCluster(cl)
+  print(Sys.time())
+  
+  # build mosaic
+  files = list.files(path, pattern = "lv95.tif", full.names = T)
+  path = dirname(files[[1]])
+  in_files = do.call(paste, c(as.list(files), sep=" "))
+  out_file = file.path(path, "ndvi_max.tif")
+  cmd = paste("gdal_merge.py -o", out_file, in_files)
+  system(cmd)
+}
--- methods/use-case1/ndvi_max_2017_switzerland.R
+++ methods/use-case1/ndvi_max_2017_switzerland.R
@@ -1,86 +1,45 @@
 ############################################################
-# Calculate NDVI max composite for Switzerland
-#
-# multi-core processing -> ca. 25min
-#
-# by Dominique Weber, HAFL, BFH
+# Calculate NDVI max composite for Switzerland.
+# 
+# by Dominique Weber, BFH-HAFL
 ############################################################
 
 start_time <- Sys.time()
 
+library(raster)
+
 # source functions
-source("/home/wbd3/sentinel2-whff/composites/calc_pixel_composites.R")
-source("/home/wbd3/sentinel2-whff/vegetation_indices/calc_vegetation_indices.R")
+source("calc_ndvi_max.R")
+source("mosaic.R")
 
-# wd
-setwd("//mnt/cephfs/data/HAFL/WWI-Sentinel-2/Data/AnnualComposites/")
+print("NDVI Max 2017")
 
-# global params
+# settings
+base_path = "//mnt/smb.hdd.rbd/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF"
 band_names = c("B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09", "B10","B11", "B12")
-dates_2017_filter = c("201706", "201707")
+year = "2017"
+months = c("06", "07")
+tiles = c("T32TLT", "T32TLS", "T32TMT", "T32TMS", "T32TNT", "T32TNS", "T32TMR", "T31TGM")
+out_path = "//mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Projekt_Use-Cases/Use-Case1"
 
-# 2017 T32TLT
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TLT/2017/"
-ndvi_max_2017_t32tlt = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t32tlt", calc_ndvi, max)
+print("processing tiles:")
+print(tiles)
+for(i in 1:length(tiles)) {
+  # prepare
+  images_path = file.path(base_path, tiles[i], year)
+  dates_filter = paste(year, months, sep="")
+  
+  # calc max composite
+  comp = calc_ndvi_max(images_path, band_names, dates_filter, ext=NULL, ind=F)
+  
+  # write
+  writeRaster(comp, file.path(out_path, paste("ndvi_max_", tiles[i], ".tif", sep="")), overwrite=T)
+}
+print(Sys.time()- start_time)
 
-# 2017 T32TLS
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TLS/2017/"
-ndvi_max_2017_t32tls = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t32tls", calc_ndvi, max)
+print("mosaic")
+Sys.time()
+mosaic(out_path)
+print(Sys.time()- start_time)
 
-# 2017 T32TMT
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TMT/2017/"
-ndvi_max_2017_t32tmt = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t32tmt", calc_ndvi, max)
-
-# 2017 T32TMS
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TMS/2017/"
-ndvi_max_2017_t32tms = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t32tms", calc_ndvi, max)
-
-# 2017 T32TNT
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TNT/2017/"
-ndvi_max_2017_t32tnt = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t32tnt", calc_ndvi, max)
-
-# 2017 T32TNS
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TNS/2017/"
-ndvi_max_2017_t32tns = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t32tns", calc_ndvi, max)
-
-# 2017 T32TMR
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T32TMR/2017/"
-ndvi_max_2017_t32tmr = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t32tmr", calc_ndvi, max)
-
-# 2017 T31TGM (-> UTM zone 31!)
-images_path = "//mnt/cephfs/data/BFH/Geodata/World/Sentinel-2/S2MSI1C/GeoTIFF/T31TGM/2017/"
-ndvi_max_2017_t31tgm = calc_pixel_composites(images_path, band_names, dates_2017_filter, "2017_t31tgm", calc_ndvi, max)
-
-# Problem different utm zones
-# There is no easy solution. We tried projectRaster to UTM32 and to LV95 but then rasters have different origins, resolution or row/col numbers
-
-# The only solution that worked so far is this (but somehow then ArcGIS had problem with the resulting mosaic):
-# reproject, extend and resample
-# ndvi_max_2017_t31tgm_32 = projectRaster(ndvi_max_2017_t31tgm, crs=crs(ndvi_max_2017_t32tls))
-# extend_32tls = extend(ndvi_max_2017_t32tls, ndvi_max_2017_t31tgm_32)
-# ndvi_max_2017_t31tgm_32_resampled = resample(ndvi_max_2017_t31tgm_32, extend_32tls)
-# mosaic all...
-
-# create mosaic
-ndvi_max_2017_ch = mosaic(ndvi_max_2017_t32tlt,
-                          ndvi_max_2017_t32tls, 
-                          ndvi_max_2017_t32tmt, 
-                          ndvi_max_2017_t32tms,
-                          ndvi_max_2017_t32tnt,
-                          ndvi_max_2017_t32tns,
-                          ndvi_max_2017_t32tmr,
-                          fun=max, 
-                          filename="ndvi_max_2017_ch.tif")
-
-# write T31TGM separately
-writeRaster(ndvi_max_2017_t31tgm, "ndvi_max_2017_t31tgm.tif")
-
-print(Sys.time() - start_time)
-
-# TODO
-# Addtional tiles - missing on the BFH server!
-#
-# T32TPT (GR)
-# T32TPS (GR)
-# T32TLR (VS)
-
+# TODO Clip to forest mask & polygonize
