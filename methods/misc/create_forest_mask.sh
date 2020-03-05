f54b01e8962046673f83cfb97895032d00447f0e
dominiqueweber <domi.wbr@gmail.com>
2020-03-05 18:27:22 +0100

    updated script to create forest mask

--- methods/misc/create_forest_mask.sh
+++ methods/misc/create_forest_mask.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # file paths
-SHP_IN=/mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Projekt_Use-Cases/general/swissTLM3D_Wald/Wald.shp
-SHP_OUT=/mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Projekt_Use-Cases/general/swissTLM3D_Wald/Wald_wgs84.shp
-RASTER_OUT=/mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Projekt_Use-Cases/general/swissTLM3D_Wald/Wald_wgs84.tif
+SHP_IN=/mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Use-Cases/general/swissTLM3D_Wald/Wald.shp
+SHP_OUT=/mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Use-Cases/general/swissTLM3D_Wald/Wald_LV95.shp
+RASTER_OUT=/mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Use-Cases/general/swissTLM3D_Wald/Wald_LV95.tif
 
-# reproject lv03 to wgs84
-ogr2ogr -s_srs EPSG:21781 -t_srs EPSG:32632 -f "ESRI Shapefile" $SHP_OUT $SHP_IN
+# reproject LV03 to LV95
+ogr2ogr -s_srs EPSG:21781 -t_srs EPSG:2056 -f "ESRI Shapefile" $SHP_OUT $SHP_IN
 
 # rasterize 
-gdal_rasterize -burn 1 -tr 10 10 -te 264850.0, 5073820.0, 609780.0, 5295110.0 -ot Byte -init 255 -a_nodata 255 -co COMPRESS=LZW $SHP_OUT $RASTER_OUT
+gdal_rasterize -burn 1 -tr 10 10 -te 2485406.16245 1075269.99667 2833846.16245 1295939.99667 -ot Byte -init 255 -a_nodata 255 -co COMPRESS=LZW $SHP_OUT $RASTER_OUT
