32b604bce1934d94a730d46705cb432a56f6c728
dominiqueweber <domi.wbr@gmail.com>
2020-03-11 14:23:05 +0100

    Use-Case 2 apply forest mask and polygonize

--- methods/use-case2/nbr_differences_main.R
+++ methods/use-case2/nbr_differences_main.R
@@ -21,9 +21,9 @@ source("use-case2/update_shapefile.R")
 # paths
 main_path = "//mnt/smb.hdd.rbd/BFH/Geodata/World/Sentinel-2/S2MSI2Ap/SAFE/"
 out_path = "//mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Use-Cases/Use-Case2/"
-shp = "//mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Use-Cases/Use-Case2/nbr_diff.shp"
+masks = "//mnt/smb.hdd.rbd/HAFL/WWI-Sentinel-2/Use-Cases/general/swissTLM3D_Wald/tiles/"
 
-tile_vec = c("T32TLT", "T32TLS", "T32TMT", "T32TMS", "T32TNT", "T32TNS", "T32TMR", "T31TGM")
+tile_vec = c("32TLT", "32TLS", "32TMT", "32TMS", "32TNT", "32TNS", "32TMR", "31TGM")
 tile_vec = tile_vec[3]
 
 # register for paralell processing
@@ -34,15 +34,19 @@ registerDoParallel(cl)
 foreach(i=1:length(tile_vec)) %dopar% {
   nbr_diff = calc_nbr_differences(main_path, out_path, tile_vec[i], year="2017", ref_date=as.Date("2017-08-19"), time_int_nbr=45, time_int_refstack=45, scl_vec=c(3,5,7:10), cloud_value=-999, nodata_value=-555)
 
-
-  # TODO apply forest mask
-
-  # polygonize
-  polys = build_polygons(nbr_diff)
-
-  # update shapefile
-  update_shapefile(polys, shp)
-
+  # apply forest mask
+  forest_mask = raster(list.files(masks, pattern=tile_vec[i], full.names = T))
+  nbr_diff_masked = mask(nbr_diff, forest_mask)
+  
+  # polygonize and update shapefile
+  shp = file.path(out_path, paste("nbr_change_", tile_vec[i], ".shp", sep=""))
+  for(j in 1:nlayers(nbr_diff_masked)){
+    # polygonize
+    polys = build_polygons(nbr_diff_masked[[j]])
+    
+    # update shapefile
+    update_shapefile(polys, shp)
+  }
 }
 
 stopCluster(cl)
