00a2e9a3363c695d0bed1ba14375908b616035cd
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-03-12 10:13:43 +0100

    check for dates when calculating differences

--- methods/use-case2/calc_diff.R
+++ methods/use-case2/calc_diff.R
@@ -4,7 +4,7 @@
 # by Dominique Weber & Alexandra Erbach, HAFL, BFH
 ############################################################
 
-calc_diff = function(stk1, stk2, cloud_value, nodata_value, time_int, out_path, tile){
+calc_diff = function(nbr_stk, comp_stk, cloud_value, nodata_value, out_path, tile){
  
   # load packages
   library(raster)
@@ -17,17 +17,25 @@ calc_diff = function(stk1, stk2, cloud_value, nodata_value, time_int, out_path, 
   registerDoParallel(cl)
   
   # build NBR stack & save NDVIs & NBRs
-  diff_stk = foreach(i=nlayers(stk1):1, .packages = c("raster"), .combine = "addLayer") %dopar% {  
+  n = nlayers(nbr_stk)
+  dates_nbr_stk = as.Date(substring(names(nbr_stk),2,9), format = "%Y%m%d")
+  dates_comp_stk = as.Date(substring(names(comp_stk),2,9), format = "%Y%m%d")
   
-    diff_tmp = stk1[[i]]-stk2[[i]]
-    diff_tmp = round(diff_tmp*100)
-    diff_tmp[(stk1[[i]] == cloud_value)] = cloud_value # clouds
-    diff_tmp[(stk1[[i]] == nodata_value)] = nodata_value # nodata
-    ras_name = paste(tile,"_NBR_diff_",substr(names(stk1[[i]]),2,9),"_",time_int,"days", sep="")
-    #cloud_perc = round(100*ncell(diff_tmp[diff_tmp==cloud_value])/(ncell(diff_tmp[diff_tmp!=nodata_value])))
+  diff_stk = foreach(i=n:1, .packages = c("raster"), .combine = "addLayer") %dopar% {  
+    
+    j = which(dates_comp_stk < dates_nbr_stk[i])[1]
+    
+    if (!is.na(j)){
+      diff_tmp = nbr_stk[[i]]-comp_stk[[j]]
+      diff_tmp[(nbr_stk[[i]] == cloud_value)] = cloud_value # clouds
+      diff_tmp[(nbr_stk[[i]] == nodata_value)] = nodata_value # nodata
+      time_int = as.integer(dates_nbr_stk[i] - dates_comp_stk[j])
+      ras_name = paste(tile,"_NBR_diff_",substr(names(nbr_stk[[i]]),2,9),"_",time_int,"days", sep="")
+      #cloud_perc = round(100*ncell(diff_tmp[diff_tmp==cloud_value])/(ncell(diff_tmp[diff_tmp!=nodata_value])))
   
-  # save as 16 Bit Integer
-    writeRaster(diff_tmp, paste(out_path, ras_name,".tif",sep=""), overwrite=T, datatype='INT2S')
+      # save as 16 Bit Integer
+      writeRaster(diff_tmp, paste(out_path, ras_name,".tif",sep=""), overwrite=T, datatype='INT2S')
+    }
   }
   
   stopCluster(cl)
