6e8a0aa29ae2bd332c334e424457b1a4dacd7dd2
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-03-16 15:56:47 +0100

    bug fix

--- methods/use-case2/calc_diff.R
+++ methods/use-case2/calc_diff.R
@@ -18,33 +18,33 @@ calc_diff = function(nbr_stk, comp_stk, cloud_value, nodata_value, out_path, til
   
   # build NBR stack & save NDVIs & NBRs
   n = nlayers(nbr_stk)
-  ind = vector(length=n)
   dates_nbr_stk = as.Date(substring(names(nbr_stk),2,9), format = "%Y%m%d")
   dates_comp_stk = as.Date(substring(names(comp_stk),2,9), format = "%Y%m%d")
   
-  diff_stk = foreach(i=n:1, .packages = c("raster"), .combine = "addLayer") %dopar% {  
-    
-    j = which(dates_comp_stk < dates_nbr_stk[i])[1]
-    diff_tmp = raster(extent(nbr_stk[[1]]), res=c(10,10))
-    
-    if (!is.na(j)){
-      ind[i] = T
-      diff_tmp = nbr_stk[[i]]-comp_stk[[j]]
-      diff_tmp[(nbr_stk[[i]] == cloud_value)] = cloud_value # clouds
-      diff_tmp[(nbr_stk[[i]] == nodata_value)] = nodata_value # nodata
-      time_int = as.integer(dates_nbr_stk[i] - dates_comp_stk[j])
-      ras_name = paste(tile,"_NBR_diff_",substr(names(nbr_stk[[i]]),2,9),"_",time_int,"days", sep="")
-      #cloud_perc = round(100*ncell(diff_tmp[diff_tmp==cloud_value])/(ncell(diff_tmp[diff_tmp!=nodata_value])))
+  ind_comp = rep(NA,n)
+  for (i in 1:n){
+    ind_comp[i] = which(dates_comp_stk < dates_nbr_stk[i])[1]
+  }
+  ind_comp = ind_comp[which(!is.na(ind_comp))]
   
-      # save as 16 Bit Integer
-      writeRaster(diff_tmp, paste(out_path, ras_name,".tif",sep=""), overwrite=T, datatype='INT2S')
-    }
+  diff_stk = foreach(i=length(ind_comp):1, .packages = c("raster"), .combine = "addLayer") %dopar% {  
+    
+    j = ind_comp[i]
+    diff_tmp = nbr_stk[[i]]-comp_stk[[j]]
+    diff_tmp[(nbr_stk[[i]] == cloud_value)] = cloud_value # clouds
+    diff_tmp[(nbr_stk[[i]] == nodata_value)] = nodata_value # nodata
+    time_int = as.integer(dates_nbr_stk[i] - dates_comp_stk[j])
+    ras_name = paste(tile,"_NBR_diff_",substr(names(nbr_stk[[i]]),2,9),"_",time_int,"days", sep="")
+    #cloud_perc = round(100*ncell(diff_tmp[diff_tmp==cloud_value])/(ncell(diff_tmp[diff_tmp!=nodata_value])))
+  
+    # save as 16 Bit Integer
+    writeRaster(diff_tmp, paste(out_path, ras_name,".tif",sep=""), overwrite=T, datatype='INT2S')
+    
+    return(diff_tmp)
   }
   
   stopCluster(cl)
-  
-  diff_stk = diff_stk[[which(ind)]]
-  
+
   return(diff_stk)
   
 }
