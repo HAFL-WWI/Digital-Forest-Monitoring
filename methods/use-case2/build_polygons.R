f35ca4fbf9a33752f94abcd34eed276b6828b553
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-03-31 09:52:25 +0200

    area calculation only for change areas (not clouds)

--- methods/use-case2/build_polygons.R
+++ methods/use-case2/build_polygons.R
@@ -13,7 +13,7 @@ build_polygons <- function(r, th = -15, area_th = 500) {
   library(stars)
   
   print("threshold and clouds...")
-  change = ((r < th) & (r > -999))
+  change = ((r < th) & (r > -555)) # careful with use of absolute number...
   change[is.na(change)] = 0
   
   print("focal filtering...")
@@ -24,19 +24,21 @@ build_polygons <- function(r, th = -15, area_th = 500) {
   # set raster values
   change[change != 1] = NA
   change[r == -999] = -1
-
+  change[r == -555] = -2
+  
   # create polygons
   print("polygonize...")
   tmp = st_as_stars(change)
   polys = st_as_sf(tmp, merge=T)
   polys = as(polys, 'Spatial')
-  test = disaggregate(polys)
+  #test = disaggregate(polys)
   names(polys) = "class"
   
   # add area and filter small polys
   print("filtering by area...")
-  polys$area = area(polys)
-  polys = polys[(((polys$area > area_th) & (polys$class==1)) | polys$class==-1) ,]
+  polys$area = NA
+  polys$area[polys$class==1] = area(polys[(polys$class==1),])
+  polys = polys[(((polys$area > area_th) & (polys$class==1)) | polys$class<0) ,]
   
   # add attributes
   polys$time = as.Date(substr(names(r), 17, 24), format = "%Y%m%d")
