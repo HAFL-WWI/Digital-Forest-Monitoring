c55e1b5ea6e1e581d0f04ac8ac91c73d0a91f43f
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2020-04-03 09:49:58 +0200

    calculate attributes (mean, max etc.) only for change areas

--- methods/use-case2/build_polygons.R
+++ methods/use-case2/build_polygons.R
@@ -14,8 +14,7 @@ build_polygons <- function(r, th = -15, area_th = 500) {
   
   print("threshold and clouds...")
   change = ((r < th) & (r > -555)) # careful with use of absolute number...
-  change[is.na(change)] = 0
-
+  
   #print("focal filtering...")
   vx = velox(change)
   vx$medianFocal(wrow=5, wcol=5, bands=1)
@@ -31,21 +30,20 @@ build_polygons <- function(r, th = -15, area_th = 500) {
   tmp = st_as_stars(change)
   polys = st_as_sf(tmp, merge=T)
   polys = as(polys, 'Spatial')
-  #test = disaggregate(polys)
   names(polys) = "class"
   
   # add area and filter small polys
   print("filtering by area...")
-  polys$area = NA
+  polys$area = polys$mean = polys$max = polys$p90 = NA
   polys$area[polys$class==1] = area(polys[(polys$class==1),])
   polys = polys[(((polys$area > area_th) & (polys$class==1)) | polys$class<0) ,]
   
   # add attributes
   polys$time = as.Date(substr(names(r), 17, 24), format = "%Y%m%d")
   vx = velox(r)
-  polys$mean = as.vector(vx$extract(sp = polys, fun = function(x) mean(x, na.rm = TRUE)))
-  polys$max = as.vector(vx$extract(sp = polys, fun = function(x) min(x, na.rm = TRUE)))
-  polys$p90 = as.vector(vx$extract(sp = polys, fun = function(x) quantile(x, 0.1, na.rm = TRUE)))
+  polys$mean[polys$class==1] = as.vector(vx$extract(sp = polys[(polys$class==1),], fun = function(x) mean(x, na.rm = TRUE)))
+  polys$max[polys$class==1] = as.vector(vx$extract(sp = polys[(polys$class==1),], fun = function(x) min(x, na.rm = TRUE)))
+  polys$p90[polys$class==1] = as.vector(vx$extract(sp = polys[(polys$class==1),], fun = function(x) quantile(x, 0.1, na.rm = TRUE)))
   
   return(polys) 
 }
