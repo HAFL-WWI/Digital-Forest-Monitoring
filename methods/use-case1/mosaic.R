650ae421f91d3705276323f271ed947fe299282e
Erbach Alexandra <eaa2@r.eaa2.bfh.science>
2021-04-07 17:48:40 +0200

    packages not needed

--- methods/use-case1/mosaic.R
+++ methods/use-case1/mosaic.R
@@ -4,10 +4,6 @@
 # by Dominique Weber, BFH-HAFL
 ############################################################
 
-# load packages
-library(foreach)
-library(doParallel)
-
 mosaic <- function(path, out, pattern_str="lv95.tif") {
   files = list.files(path, pattern = pattern_str, full.names = T)
   path = dirname(files[[1]])
