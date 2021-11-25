ccfb195798123357be9c4c2d299f6a0167da61b4
Hanskaspar Frei <hkfrei@karten-werk.ch>
2021-11-25 15:13:51 +0100

    feat: add logic for the vhm info button.

--- webapp/src/utils/VHMControl.js
+++ webapp/src/utils/VHMControl.js
@@ -1,10 +1,18 @@
 import { Control } from "ol/control";
 import { vegetationBasemap } from "./basemap_util";
 import { updateUrl, removeParam } from "./url_util";
+import {
+  openSidebar,
+  sidebar,
+  closeSidebar,
+  clearSidebar,
+  GEO_ADMIN_WMS_INFO_URL
+} from "./main_util";
 const vegetationImage = new URL(
   "../img/basemapVegetation.jpg",
   import.meta.url
 );
+const infoIcon = new URL("../img/info_black_24dp.svg", import.meta.url);
 class VHMControl {
   constructor(map = null, showVegetation = false) {
     this.map = map;
@@ -17,7 +25,32 @@ class VHMControl {
     this.vhmControl.style.backgroundImage = `url(${vegetationImage})`;
     const vhmTitle = document.createElement("div");
     vhmTitle.classList.add("vhmControl__title");
-    vhmTitle.innerHTML = "VHM";
+    const vhmInfo = document.createElement("img");
+    vhmInfo.src = infoIcon;
+    vhmInfo.alt = "layer infos";
+    vhmInfo.className = "vhmControl__info";
+    vhmInfo.addEventListener("click", e => {
+      e.stopPropagation();
+      if (sidebar.dataset.open === "false") {
+        // open the sidebar for an immediate user experience
+        const waiting = document.createElement("div");
+        waiting.classList.add("vhmControl__waiting");
+        waiting.style.paddingTop = "32px";
+        waiting.innerHTML = "Loading...<br /><br /> Bitte einen Moment Geduld.";
+        openSidebar({ content: waiting });
+        // populate the sidebar with content
+        this.getVHMInfoContent().then(content => {
+          clearSidebar();
+          openSidebar({ content });
+        });
+      } else {
+        closeSidebar();
+      }
+    });
+    const buttonTitle = document.createElement("span");
+    buttonTitle.textContent = "VHM";
+    vhmTitle.appendChild(buttonTitle);
+    vhmTitle.appendChild(vhmInfo);
     this.vhmText = document.createElement("div");
     this.vhmText.classList.add("vhm__text");
     this.vhmText.innerHTML = "Turn on";
@@ -62,6 +95,44 @@ class VHMControl {
     vegetationBasemap.setVisible(false);
     this.vhmText.innerHTML = "Turn on";
     removeParam("vhm");
+  }
+
+  async getVHMInfoContent() {
+    const info = document.createElement("div");
+    const title = document.createElement("h3");
+    try {
+      const response = await fetch(
+        `${GEO_ADMIN_WMS_INFO_URL}layername=ch.bafu.landesforstinventar-vegetationshoehenmodell`
+      );
+      const json = await response.json();
+      title.textContent = json.layer.title[0];
+      info.appendChild(title);
+      const legendUrl =
+        json?.layer?.style[0]?.LegendURL[0]?.OnlineResource[0]?.$[
+          "xlink:href"
+        ] ||
+        "https://api.geo.admin.ch/static/images/legends/ch.bafu.landesforstinventar-vegetationshoehenmodell_de.png";
+
+      const legend = document.createElement("h4");
+      legend.textContent = "Legende:";
+      const legendImage = document.createElement("img");
+      legendImage.src = legendUrl;
+      legendImage.alt = "vhm legende";
+      info.appendChild(legend);
+      info.appendChild(legendImage);
+      const description = document.createElement("h4");
+      description.textContent = "Beschreibung:";
+      info.appendChild(description);
+      const abstract = document.createElement("div");
+      abstract.innerText = json?.layer?.abstract[0];
+      info.appendChild(abstract);
+      return info;
+    } catch (error) {
+      const errorContent = document.createElement("div");
+      errorContent.innerHTML = `Es gab einen Fehler beim Laden der Layer Infos: <hr /> ${error} <hr /> Bitte versuchen sie es später nochmals.`;
+      info.appendChild(errorContent);
+      return info;
+    }
   }
 }
 export default VHMControl;
