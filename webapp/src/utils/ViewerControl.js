8d9fadf438964bb70456287555bbff7bec4633e1
hkfrei <hkfrei@bluewin.ch>
2023-09-20 08:36:41 +0200

    feat: add vitality layers for 2023

--- webapp/src/utils/ViewerControl.js
+++ webapp/src/utils/ViewerControl.js
@@ -177,6 +177,7 @@ class ViewerControl {
       { number: "12-01", text: "Dez/Jan" }
     ];
     this.vitalityLayers = [
+      { year: "2023", month: this.month.slice(5, 8), layers: [] },
       { year: "2022", month: this.month.slice(5, 8), layers: [] },
       { year: "2021", month: this.month.slice(5, 8), layers: [] },
       { year: "2020", month: this.month.slice(5, 8), layers: [] },
