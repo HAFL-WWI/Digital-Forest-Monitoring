ad4067a0c27659c263a99adc63c95c9754a6359a
Hannes Horneber <hannes.horneber@bfh.ch>
2023-10-24 13:36:06 +0200

    Fix: gee script now uses S2_Harmonized to reflect radiometric offset update 25.01.2022

--- methods/use-case1/1_ndvi_max_gee_script.js
+++ methods/use-case1/1_ndvi_max_gee_script.js
@@ -3,16 +3,18 @@
 //
 // GEE -> Google Earth Engine -> https://code.earthengine.google.com/38acfd5a95ddca198e7f2b134e1e4664
 //
-// Script prepared by Dominique Weber, BFH-HAFL
+// by Dominique Weber, Hannes Horneber, BFH-HAFL
 /////////////////////////////////////////////////////////////
 
 Map.setCenter(8.83, 47.62, 7)
 var swiss = ee.Geometry.Rectangle(5.4, 45.5, 11, 48.1);
 var aoi = swiss;
 
-// Sentine-2 imagery --> S2 for L1C, S2_SR for L2A
-var S2 = ee.ImageCollection('COPERNICUS/S2')
-  .filterDate('2019-06-01', '2019-09-01')
+// Sentine-2 imagery --> S2 (for L1C), S2_SR (for L2A)
+// data after 25.01.2022 is incompatible due to the processing baseline update 04.00
+// use S2_HARMONIZED (L1C) or S2_SR_HARMONIZED (L2A), which remove the baseline offset
+var S2 = ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
+  .filterDate('2023-06-01', '2023-09-01')
   .filterBounds(aoi);
 
 // add layers
@@ -28,7 +30,7 @@ var greenest = S2.qualityMosaic('NDVI');
 
 // display
 Map.addLayer(greenest.select('NDVI'), {min: 0, max: 1, palette: ['red', 'blue', 'green']}, 'NDVI max');
-Map.addLayer(greenest, {bands: ['B4', 'B3', 'B2'], max: 1500}, 'Greenest pixel composite');
+//Map.addLayer(greenest, {bands: ['B4', 'B3', 'B2'], max: 1500}, 'Greenest pixel composite');
 
 // Export
 var ndvi_max = greenest.select('NDVI');
